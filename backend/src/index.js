2cdfe6332454bd550ac1fa119120efd34c1e62c9
Oledev827 <kingdev1989827@gmail.com>
2019-03-28 10:44:09 +0800

    sign out button and readme

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,28 @@
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+const db = require('./db')
 
-const server = createServer();
+const server = createServer()
 
-// Use express middlware to handle cookies (JWT)
-server.express.use(cookieParser());
-// TODO Use express middlware to populate current user
+server.express.use(cookieParser())
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    // put the userId onto the req for future requests to access
+    req.userId = userId
+  }
+  next()
+})
 
 server.start(
-    {
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
-    },
-    },
-    deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
-    }
-);
+  { cors: { credentials: true, origin: process.env.FRONTEND_URL } },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`)
+  }
+)
